738d23858ba8a4e85b0169566d1707c387810a08
juliabeatriz09 <159266648+juliabeatriz09@users.noreply.github.com>
2024-09-19 12:13:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function calcula();
+const salario (document.getElementById("salario"));
+const base = salario - descontos; 
+const aliquota (document.getElementById("aliquota"));
+let deducao = ;
+const principal = base * aliquota - deducao; 
+if{
+
+}
