291dda373f7985be1da883c206b0e94141d8b8b6
juliabeatriz09 <159266648+juliabeatriz09@users.noreply.github.com>
2024-09-26 12:13:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-// julia 
 function irrf(){
-const salario = document.getElementById('salario').value;
-const base = salario - descontos; 
-let aliquota=0;
-const num_dependentes = document.getElementById('dependentes').value;
-let deducao= document.getElementById('deducao').value*document.getElementById('dependentes').value;
-const principal = base * aliquota - deducao; 
-let result = principal;
-
-
-
- if (salario<=2259,20){
-     getElementById('valor').innerhtml="não é necessário pagar imposto";
- }else if( salario<=2826,65){
-    aliquota=7.5*salario/100
-    result = principal;
-    getElementById('valor').innerhtml= "o valor a ser pago=" + result;
- }else if( salario<=3751,65){
-    aliquota=15.0*salario/100
-    result = principal;
-    getElementById('valor').innerhtml= "o valor a ser pago=" + result;
- }else if( salario<=4664,68){
-    aliquota=22.5*salario/100
-    result = principal;
-    getElementById('valor').innerhtml= "o valor a ser pago=" + result;
- }else{
-    aliquota=27.5*salario/100
-    result = principal;
-    getElementById('valor').innerhtml= "o valor a ser pago=" + result;
- }
+   
+    const salario = document.getElementById('salario').value;
+    const base = salario - document.getElementById('deducao').value; 
+    let aliquota = 0;
+    const num_dependentes = document.getElementById('dependentes').value;
+    let deducao = document.getElementById('deducao').value * document.getElementById('dependentes').value;
+    let result = 0;
+    
+    
+     if (salario<=2259.20){
+         document.getElementById('valor').innerText="não é necessário pagar imposto";
+     }else if( salario<=2826.5){
+        aliquota=7.5*salario/100;
+        result = base * aliquota -deducao;
+        
+     }else if( salario<=3751.65){
+        aliquota=15.0*salario/100;
+        result = base * aliquota -deducao;
+        
+     }else if( salario<=4664.68){
+        aliquota=22.5*salario/100;
+        result = base * aliquota -deducao;
+        
+     }else{
+        aliquota=27.5*salario/100;
+        result = base * aliquota -deducao;
+        
+     }
+     document.getElementById('valor').innerText = "o valor a ser pago=" + result;
 }
