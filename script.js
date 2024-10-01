905f986f19699b66247b4d501c34223c09078372
juliabeatriz09 <159266648+juliabeatriz09@users.noreply.github.com>
2024-10-01 15:04:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 function irrf(){
    
-    const salario = document.getElementById('salario').value;
+    const salario = document.getElementById('salario').value - 554.8;
     const base = salario - document.getElementById('deducao').value; 
     let aliquota = 0;
+    let deducao =0;
     const num_dependentes = document.getElementById('dependentes').value;
-    let deducao = document.getElementById('deducao').value * document.getElementById('dependentes').value;
     let result = 0;
     
     
      if (salario<=2259.20){
          document.getElementById('valor').innerText="não é necessário pagar imposto";
      }else if( salario<=2826.5){
-        aliquota=0.075*salario/100;
+        deducao =( 169.44 * num_dependentes) / num_dependentes;
+        aliquota=(0.075*salario)/100;
         result = (base * aliquota) -deducao;
+       
         
      }else if( salario<=3751.65){
-        aliquota=0.15*salario/100;
+        deducao = (381.44 * document.getElementById('dependetes').value) / document.getElementById('dependentes');
+        aliquota=(0.15*salario)/100;
         result = (base * aliquota )-deducao;
         
      }else if( salario<=4664.68){
-        aliquota=22.5*salario/100;
+        deducao = (662.77 * document.getElementById('dependetes').value) / document.getElementById('dependentes');
+        aliquota = (0.225*salario)/100;
         result = (base * aliquota )-deducao;
         
      }else{
-        aliquota=27.5*salario/100;
+        deducao = (896.0 * document.getElementById('dependetes').value) / document.getElementById('dependentes');
+        aliquota = (0.275*salario)/100;
         result = (base * aliquota) -deducao;
         
      }
