183e3b4fc7a1336335ce627c77e1dc5baf535ff4
juliabeatriz09 <159266648+juliabeatriz09@users.noreply.github.com>
2024-10-03 11:47:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function irrf(){
    // declaração de variaveis
     const salario = document.getElementById('salario').value - 554.8;
-    const base = salario - document.getElementById('deducao').value; 
+    let base = 0 ; 
     let aliquota = 0;
     let num_depe = document.getElementById('dependentes').value;
-    let deducao =(document.getElementById('deducao').value * num_depe) / num_depe ;
+    let deducao =0;
     let result = 0;
   
    //  fim declaração
@@ -13,22 +13,33 @@ function irrf(){
          document.getElementById('valor').innerText="não é necessário pagar imposto";
 
      }else if( salario<=2826.5){ 
+        deducao = (169.44 * num_depe) / num_depe;
+        base = salario - deducao;
         aliquota=(0.075 * salario)/100;
         result = (base * aliquota) - deducao;
-        
+        document.getElementById('valor').innerText =   result.toFixed(2);
+
      }else if( salario<=3751.65){
+        deducao= (381.44 * num_depe) / num_depe;
+        base = salario -  deducao;
         aliquota=(0.15 * salario)/100;
         result = (base * aliquota )- deducao;
+        document.getElementById('valor').innerText =  result.toFixed(2);
        
      }else if( salario<=4664.68){
+        deducao= (662.77 * num_depe) / num_depe;
+        base = salario - deducao;
         aliquota = (0.225 * salario)/100;
         result = (base * aliquota )- deducao;
+        document.getElementById('valor').innerText = result.toFixed(2);
         
      }else{
+        deducao = (896.00 * num_depe) / num_depe;
+        base = salario - deducao;
         aliquota = (0.275 * salario)/100;
         result = (base * aliquota) - deducao;
-        
+        document.getElementById('valor').innerText =  result.toFixed(2);
      }
    //   fim calculo
-     document.getElementById('valor').innerText = "o valor a ser pago=" + result.toFixed(2);
+     
 }
