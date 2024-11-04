5fa5f7c7d8a9aae8df8aa60a4b60eacb64802bd1
juliabeatriz09 <159266648+juliabeatriz09@users.noreply.github.com>
2024-11-04 12:10:05 -0300

    Add files via upload

---
+++ quizz.js
@@ -0,0 +1,16 @@
+function quizz(){
+    var  resp1= document.querySelector('input[name="quest1"]:checked') ;
+    var  resp2= document.querySelector('input[name="quest2"]:checked') ;
+    var  resp3= document.querySelector('input[name="quest3"]:checked') ;
+    var  resp4= document.querySelector('input[name="quest4"]:checked') ;
+    var  resp5= document.querySelector('input[name="quest5"]:checked') ;
+    var  resp6= document.querySelector('input[name="quest6"]:checked') ;
+    var  resp7= document.querySelector('input[name="quest7"]:checked') ;
+    var  resp8= document.querySelector('input[name="quest8"]:checked') ;
+    var  resp9= document.querySelector('input[name="quest9"]:checked') ;
+    var  resp10= document.querySelector('input[name="quest10"]:checked') ;
+
+   if (resp1.value== 'correta'){
+
+   }
+}
