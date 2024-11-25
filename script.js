cbe24f1cfdfe0e763b608a1310a5b05c6153a1f6
Anna Clara <159267164+annaclaralemossilva@users.noreply.github.com>
2024-11-25 08:47:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
+         document.getElementById('valor').innerText="Não é necessário pagar imposto";
 
-let slideIndex = 0;
+     }else if( salario<=2826.5){ 
+        deducao = 169.44  * num_depe;
+        deducao = 189,59  * num_depe;
+        base = salario - deducao;
+        aliquota=(7.50 * salario)/100;
+        result = (base * aliquota) - deducao;
+        aliquota=(7.50 * base)/100;
+        result = aliquota - 169,44;
+        document.getElementById('valor').innerText =   result.toFixed(1);
 
-function mostrarSlide(index) {
-    const slides = document.querySelector('.slides');
-    const imagens = document.querySelectorAll('.slides img');
-    
-    if (index >= imagens.length) {
-        slideIndex = 0;
-    } else if (index < 0) {
-        slideIndex = imagens.length - 1;
-    } else {
-        slideIndex = index;
-    }
-    
-    slides.style.transform = `translateX(${-slideIndex * 100}%)`;
-}
+     }else if( salario<=3751.65){
+        deducao= 381.44  * num_depe;
+        deducao= 189,59  * num_depe;
+        base = salario -  deducao;
+        aliquota=(0.15 * salario)/100;
+        result = (base * aliquota )- deducao;
+        aliquota=(15 * base)/100;
+        result = aliquota - 381,44;
+        document.getElementById('valor').innerText =  result.toFixed(1);
 
-function mudarSlide(n) {
-    mostrarSlide(slideIndex + n);
-}
+     }else if( salario<=4664.68){
+        deducao= 662.77 * num_depe;
+        deducao= 189,59 * num_depe;
+        base = salario - deducao;
+        aliquota = (0.225 * salario)/100;
+        result = (base * aliquota )- deducao;
+        aliquota = (22.5 * base)/100;
+        result =  aliquota - 662,77;
+        document.getElementById('valor').innerText = result.toFixed(1);
 
-// Inicia o carrossel
-mostrarSlide(slideIndex);
-
+     }else{
+        deducao = 896.00  * num_depe;
+        deducao = 189,59  * num_depe;
+        base = salario - deducao;
+        aliquota = (0.275 * salario)/100;
+        result = (base * aliquota) - deducao;
+        aliquota = (27.5 * base)/100;
+        result = aliquota- 896;
+        document.getElementById('valor').innerText =  result.toFixed(1);
+     }
+   //   fim calculo
 
 
